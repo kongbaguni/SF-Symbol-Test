26d35b2aa8919114ec2e5788297e49f6d99f1dd5
chang-yul, Seo <kongbaguni@gmail.com>
2022-12-12 17:31:17 +0900

    Initial Commit

---
+++ SFSymbolTest/SFSymbolTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolTestApp.swift
+//  SFSymbolTest
+//
+//  Created by 서창열 on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
