f8886f721f2d5584c39b9a58351c0a79110d919c
chang-yul, Seo <kongbaguni@gmail.com>
2023-01-12 15:54:27 +0900

    이미지 공유하기 고도화. 배경색상, 사이즈 선택 기능 구현

--- SFSymbolTest/View/SFSymbolDetailView.swift
+++ SFSymbolTest/View/SFSymbolDetailView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct SFSymbolDetailView: View {
+    enum ActionSheetType {
+        case selectShareImageSize
+        case selectBackgroundColor
+    }
     let imageName:String
     @Binding var optionData:OptionView.Data
     @State var isToast = false
     @State var toastMessage = ""
     @State var toastTitle:Text? = nil
+    @State var isShowActionSheet = false
+    @State var actionSheetType:ActionSheetType? = nil
+    @State var bgColor:Color = .white
     let ad = GoogleFullScreenAd()
     var body: some View {
         List {
@@ -21,13 +28,8 @@ struct SFSymbolDetailView: View {
             Section {
                 Button {
                     ad.showAd { sucess, time in
-                        let image = Image(systemName: imageName)
-                            .symbolRenderingMode(optionData.renderingMode)
-                            .foregroundStyle(optionData.forgroundColor.0,optionData.forgroundColor.1,optionData.forgroundColor.2)
-                            .font(.system(size: 200,weight: optionData.fontWeight))
-                            .frame(width: 300,height: 300)
-                            .asUIImage()
-                        showShareSheet(with: [image])
+                        isShowActionSheet = true
+                        actionSheetType = .selectBackgroundColor
                     }
                     
                 } label: {
@@ -52,7 +54,79 @@ struct SFSymbolDetailView: View {
         }
         .navigationTitle(imageName)
         .toast(title: toastTitle, message: toastMessage, isShowing: $isToast, duration: 4)
-        
+        .actionSheet(isPresented: $isShowActionSheet) {
+            switch actionSheetType{
+                case .selectBackgroundColor:
+                    return .init(title: Text("select background Color"), buttons: [
+                        .default(Text("black"),action: {
+                            selectBgColor(color: .black)
+                        }),
+                        .default(Text("white"),action: {
+                            selectBgColor(color: .white)
+                        }),
+                        .default(Text("yellow"),action: {
+                            selectBgColor(color: .yellow)
+                        }),
+                        .default(Text("orange"),action: {
+                            selectBgColor(color: .orange)
+                        }),
+                        .default(Text("green"),action: {
+                            selectBgColor(color: .green)
+                        }),
+                        .default(Text("mint"),action: {
+                            selectBgColor(color: .mint)
+                        }),
+                        .default(Text("gray"),action: {
+                            selectBgColor(color: .gray)
+                        }),
+                        .cancel()
+                    ])
+                case .selectShareImageSize:
+                        return .init(title: Text("select share image Size"),
+                              buttons: [
+                                .default(Text("share_small_title"),action: {
+                                    shareImage(sizes: [200])
+                                }),
+                                .default(Text("share_medeum_title"), action: {
+                                    shareImage(sizes: [400])
+                                }),
+                                .default(Text("share_large_title"), action: {
+                                    shareImage(sizes: [600])
+                                }),
+                                .default(Text("share_all_title"), action: {
+                                    shareImage(sizes: [200,400,600])
+                                }),
+
+                                .cancel()
+                        ])
+                
+                default:
+                    return .init(title: Text("error"))
+            }
+
+        }
+    }
+    private func selectBgColor(color:Color) {
+        bgColor = color
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
+            isShowActionSheet = true
+            actionSheetType = .selectShareImageSize
+        }
+    }
+    
+    private func shareImage(sizes:[CGFloat]) {
+        var result:[Any] = []
+        for size in sizes {
+            let image = Image(systemName: imageName)
+                .symbolRenderingMode(optionData.renderingMode)
+                .foregroundStyle(optionData.forgroundColor.0,optionData.forgroundColor.1,optionData.forgroundColor.2)
+                .font(.system(size: size * 0.8 ,weight: optionData.fontWeight))
+                .frame(width: size,height: size)
+                .background(bgColor)
+                .asUIImage()
+            result.append(image)
+        }
+        showShareSheet(with: result)
     }
 }
 
