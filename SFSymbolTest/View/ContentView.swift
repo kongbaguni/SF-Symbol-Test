205e32b733c295c2f64ab1d6c4047b6c79f87372
서창열 <kongbaguni@gmail.com>
2023-07-20 10:11:46 +0900

    구글 광고 사용 위한 AppTraking 프롬프트 관련 코드 정리

--- SFSymbolTest/View/ContentView.swift
+++ SFSymbolTest/View/ContentView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import GoogleMobileAds
-import UserMessagingPlatform
 
 
 struct ContentView: View {
@@ -15,67 +14,11 @@ struct ContentView: View {
     
     
     init() {
-        #if DEBUG
-        UMPConsentInformation.sharedInstance.reset()
-        #endif
-
-        GoogleAd.requestTrackingAuthorization {[self] in 
-            ump()
+        GoogleAdPrompt.promptWithDelay {
+            
         }
     }
     
-    func ump() {
-        func loadForm() {
-          // Loads a consent form. Must be called on the main thread.
-            UMPConsentForm.load { form, loadError in
-                if loadError != nil {
-                  // Handle the error
-                } else {
-                    // Present the form. You can also hold on to the reference to present
-                    // later.
-                    if UMPConsentInformation.sharedInstance.consentStatus == UMPConsentStatus.required {
-                        form?.present(
-                            from: UIApplication.shared.lastViewController!,
-                            completionHandler: { dismissError in
-                                if UMPConsentInformation.sharedInstance.consentStatus == UMPConsentStatus.obtained {
-                                    // App can start requesting ads.
-                                }
-                                // Handle dismissal by reloading form.
-                                loadForm();
-                            })
-                    } else {
-                        // Keep the form available for changes to user consent.
-                    }
-                    
-                }
-
-            }
-        }
-        // Create a UMPRequestParameters object.
-        let parameters = UMPRequestParameters()
-        // Set tag for under age of consent. Here false means users are not under age.
-        parameters.tagForUnderAgeOfConsent = false
-        #if DEBUG
-        let debugSettings = UMPDebugSettings()
-//        debugSettings.testDeviceIdentifiers = ["78ce88aff302a5f4dfa5226a766c0b5a"]
-        debugSettings.geography = UMPDebugGeography.EEA
-        parameters.debugSettings = debugSettings
-        #endif
-        UMPConsentInformation.sharedInstance.requestConsentInfoUpdate(
-            with: parameters,
-            completionHandler: { error in
-                if error != nil {
-                    // Handle the error.
-                    print(error!.localizedDescription)
-                } else {
-                    let formStatus = UMPConsentInformation.sharedInstance.formStatus
-                    if formStatus == UMPFormStatus.available {
-                      loadForm()
-                    }
-
-                }
-            })
-    }
     
     var body: some View {
         NavigationView {
